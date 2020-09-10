99fe221eea0ef7a4969ce55adc89d00c5fc7848f
AnnieChien <wyctueps@gmail.com>
2020-09-10 14:36:58 +0800

    demo GTX

--- external/custom/artwork.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-	$( "#release_date" ).datepicker({ dateFormat: 'yy-mm-dd 00:00:00' });
-});
--- external/custom/widget_header.js
+++
@@ -1,9 +0,0 @@
-function setLanguage(language) {
-    if (language) {
-        var d = new Date();
-        d.setTime(d.getTime() + (365*24*60*60*1000));
-        var expires = "expires="+ d.toUTCString();
-        document.cookie = "language=" + language + ";" + expires + ";path=/";
-        location.reload();
-    }
-};
